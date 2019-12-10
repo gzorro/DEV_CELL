fe21ea00613004845fb7be0547f0c7f80ff74e45
gzorro@intergrupo.com <gzorro@intergrupo.com>
2019-12-10 13:39:45 -0500

    Se crean componentes header, home, search y user

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SearchComponent } from './components/search/search.component';
+import { UserComponent } from './components/user/user.component';
 
-
-const routes: Routes = [
-  { path: 'home', component: HomeComponent, pathMatch: 'full' }
+const ROUTES: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'header', component: HeaderComponent },
+  { path: 'search', component: SearchComponent },
+  { path: 'user', component: UserComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'home' }
 
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  // imports: [RouterModule.forRoot(ROUTES)],
+  imports: [RouterModule.forRoot(ROUTES, { useHash: true})],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class APPROUTINGMODULE { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { APPROUTINGMODULE } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './components/home/home.component';
 import { SearchComponent } from './components/search/search.component';
+import { UserComponent } from './components/user/user.component';
 
 @NgModule({
+  // exports:[]
   declarations: [
     AppComponent,
     HeaderComponent,
     HomeComponent,
-    SearchComponent
+    SearchComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    APPROUTINGMODULE
   ],
   providers: [],
   bootstrap: [AppComponent]
